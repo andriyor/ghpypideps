efc1892da337c76ab6bbf9d429db45c2133f080f
Andrii Oriekhov <andriyorehov@gmail.com>
2022-03-13 17:54:02 +0200

    parse setup.py AST

--- ghpypideps/ghpypideps.py
+++ ghpypideps/ghpypideps.py
@@ -1,5 +1,6 @@
 import json
 import os
+import ast
 # [python - Proper way to parse requirements file after pip upgrade to pip 10.x.x? - Stack Overflow](https://stackoverflow.com/questions/49689880/proper-way-to-parse-requirements-file-after-pip-upgrade-to-pip-10-x-x)
 from pip._internal.network.session import PipSession
 from pip._internal.req import parse_requirements
@@ -15,10 +16,92 @@ from source_finder import find_source_repo
 
 config = configparser.ConfigParser()
 SETUP_CFG = 'setup.cfg'
+SETUP_PY = 'setup.py'
 OPTIONS_EXTRAS_REQUIRE = 'options.extras_require'
 
 token = os.environ.get('GITHUB_TOKEN')
 github = github3.login(token=token)
+
+
+# PyNaCl
+# Flask
+class Analyzer(ast.NodeVisitor):
+    def __init__(self):
+        self.assigns = {}
+        self.req = {}
+
+    def visit_Assign(self, node):
+        if isinstance(node.targets[0], ast.Name):
+            if isinstance(node.value, ast.List):
+                constants = []
+                for elr in node.value.elts:
+                    if isinstance(elr, ast.Constant):
+                        constants.append(elr.value)
+                self.assigns[node.targets[0].id] = constants
+            
+            if isinstance(node.value, ast.Dict):
+                constants = []
+                for val in node.value.values:
+                    # python-cloud-core extras
+                    if isinstance(val, ast.Constant):
+                        constants.append(val.value)
+                    
+                    # attrs
+                    if isinstance(val, ast.List):
+                        for elr in val.elts:
+                            if isinstance(elr, ast.Constant):
+                                constants.append(elr.value)
+                self.assigns[node.targets[0].id] = constants
+
+    def visit_Call(self, node):
+        is_setuptools_setup = isinstance(node.func, ast.Attribute) and hasattr(node.func.value, 'id') and node.func.value.id == 'setuptools' and node.func.attr == 'setup'
+        is_setup = isinstance(node.func, ast.Name) and (node.func.id == 'setup' or is_setuptools_setup)
+        if is_setup or is_setuptools_setup:
+            assigns_keys = self.assigns.keys()
+            for key in node.keywords:
+                # TODO: refactor
+                if key.arg == 'tests_require':
+                    if isinstance(key.value, ast.Name) and key.value.id in assigns_keys:
+                        self.req['tests_require'] = self.assigns[key.value.id]
+
+                    if isinstance(key.value, ast.List):
+                        self.req['tests_require'] = [elt.value for elt in key.value.elts]
+                
+                if key.arg == 'setup_requires':
+                    if isinstance(key.value, ast.Name) and key.value.id in assigns_keys:
+                        self.req['setup_requires'] = self.assigns[key.value.id]
+
+                    if isinstance(key.value, ast.List):
+                        self.req['setup_requires'] = [elt.value for elt in key.value.elts]
+                
+                if key.arg == 'install_requires':
+                    # handle BinOp? https://github.com/matplotlib/matplotlib/blob/main/setup.py#L308
+                    if isinstance(key.value, ast.Name) and key.value.id in assigns_keys:
+                        self.req['install_requires'] = self.assigns[key.value.id]
+
+                    if isinstance(key.value, ast.List):
+                        self.req['install_requires'] = [elt.value for elt in key.value.elts]
+
+                # TODO: use dict for kkeys?
+                if key.arg == 'extras_require':
+                    # python-cloud-core
+                    if isinstance(key.value, ast.Name) and key.value.id in assigns_keys:
+                        self.req['extras_require'] = self.assigns[key.value.id]
+
+                    if isinstance(key.value, ast.Dict):
+                        extras_require = []
+                        for val in key.value.values:
+                            # nacl
+                            if isinstance(val, ast.Name):
+                                extras_require.extend(self.assigns[val.id])
+                            if isinstance(val, ast.List):
+                                # Flask
+                                extras_require.extend([elt.value for elt in val.elts])
+
+                        self.req['extras_require'] = extras_require
+
+    def flat(self):
+        return [item for sublist in self.req.values() for item in sublist]
 
 
 def handle_requirements(uritemplate, path):
@@ -137,9 +220,21 @@ def fetch_deps(package_name):
 
             if content_obj.name == SETUP_CFG:
                 print(content_obj.path)
-                setup_req = handle_setup_cfg(uritemplate)
-                print(setup_req)
-                all_req[SETUP_CFG] = setup_req
+                setup_cfg_req = handle_setup_cfg(uritemplate)
+                print(setup_cfg_req)
+                all_req[SETUP_CFG] = setup_cfg_req
+
+            if content_obj.name == SETUP_PY:
+                print(content_obj.path)
+                req = uritemplate.file_contents(SETUP_PY).decoded.decode('utf-8')
+
+                tree = ast.parse(req)
+                analyzer = Analyzer()
+                analyzer.visit(tree)
+                setup_py_req = analyzer.flat()
+                print(setup_py_req)
+                print()
+                all_req[SETUP_PY] = setup_py_req
 
     requires_dist = get_from_pypi(package_name)
     all_req['pypi'] = requires_dist
@@ -180,8 +275,23 @@ if __name__ == "__main__":
     # package_name = 'scipy'
     # package_name = 'Pillow'
     # package_name = 'pluggy'
-    package_name = 'tqdm'
-    deps = fetch_deps(package_name)
+    # package_name = 'tqdm'
+    # package_name = 'GitPython'
+    # package_name = 'gunicorn'
+    # package_name = 'PyNaCl'
+    package_name = 'requests'
 
+    deps = fetch_deps(package_name)
     with open(f'tests/results/{package_name}.json', 'w') as outfile:
-        json.dump(deps, outfile, indent=4)
+        json.dump(deps, outfile, indent=2)
+
+
+    # with open("examples/setup_mat.py", "r") as source:
+    #     tree = ast.parse(source.read())
+    #     print(ast.dump(tree, indent=4))
+
+    # analyzer = Analyzer()
+    # analyzer.visit(tree)
+    # print(analyzer.assigns)
+    # print(analyzer.req)
+    # print(analyzer.flat())
